53d10e6c02a040ae9f07817515bd4c0b89ffd296
vitalii <0x1d46@gmail.com>
2025-04-12 11:00:15 +0200

    Refactor image resizing logic and add tile calculation function

--- index.ts
+++ index.ts
@@ -6,13 +6,15 @@ import { mkdir, exists, rm } from "node:fs/promises";
 import sharp from "sharp";
 
 const uploadDir = "images";
-const maxSize = 1500;
 const rand = () => Math.random().toString(24).slice(2, 8);
 const port = process.env.PORT || 3000;
 const route = "/image";
 const destination = join(__dirname, uploadDir);
 
 const upload = multer({
+  limits: {
+    fileSize: 9999999999,
+  },
   storage: diskStorage({
     destination,
     filename: (_, f, cb) => cb(null, rand() + extname(f.originalname)),
@@ -24,24 +26,15 @@ const uploadHandler = async (rq: Request, rs: Response) => {
   else {
     const image = sharp(rq.file.path);
     const { width, height, format } = await image.metadata();
-    let resize: Array<number | null>;
 
-    if (Math.max(width!, height!) > maxSize) {
-      if (width! > height!) {
-        resize = [maxSize, null];
-      } else {
-        resize = [null, maxSize];
-      }
+    const filename = `${rand()}.${format}`;
+    const filepath = join(__dirname, "images", filename);
+    await image
+      .resize(resizeImageIfNeeded(width!, height!, 512, 4))
+      .toFile(filepath);
+    await rm(rq.file.path);
 
-      const filename = `${rand()}.${format}`;
-      const filepath = join(__dirname, "images", filename);
-      await image.resize(...resize).toFile(filepath);
-      await rm(rq.file.path);
-
-      rs.json(`${rq.protocol}://${rq.get("host")}/image/${filename}`);
-    } else {
-      rs.json(`${rq.protocol}://${rq.get("host")}/image/${rq.file.filename}`);
-    }
+    rs.json(`${rq.protocol}://${rq.get("host")}/image/${filename}`);
   }
 };
 
@@ -62,3 +55,28 @@ express()
   .post(route, upload.single("image"), uploadHandler)
   .delete(route, deleteHandler)
   .listen(port, console.info);
+
+function calculateTiles(width: number, height: number, tileSize: number) {
+  const tilesWide = Math.ceil(width / tileSize);
+  const tilesHigh = Math.ceil(height / tileSize);
+  return tilesWide * tilesHigh;
+}
+
+function resizeImageIfNeeded(
+  width: number,
+  height: number,
+  tileSize: number,
+  maxTiles: number,
+) {
+  const currentTileCount = calculateTiles(width, height, tileSize);
+
+  if (currentTileCount > maxTiles) {
+    const scaleFactor = Math.sqrt(currentTileCount / maxTiles);
+    const newWidth = Math.floor(width / scaleFactor);
+    const newHeight = Math.floor(height / scaleFactor);
+
+    return { width: newWidth, height: newHeight };
+  } else {
+    return { width, height };
+  }
+}
